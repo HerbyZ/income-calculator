05968bca6d8e89581310b0b79663c021da545b35
HerbyZ <andreyherby@gmail.com>
2024-12-20 19:59:27 +0300

    Output showcase

---
+++ src/main.rs
@@ -0,0 +1,78 @@
+struct Position {
+    name: String,
+    amount: f64,
+    value: f64,
+    buy_price: f64,
+    sell_price: f64,
+    income: f64,
+}
+
+fn get_position_info() -> Position {
+    let mut name_input = String::new();
+    let mut amount_input = String::new();
+    let mut value_input = String::new();
+
+    let stdin = std::io::stdin();
+
+    println!("Enter position name:");
+    stdin
+        .read_line(&mut name_input)
+        .expect("Failed to read name");
+
+    println!("Enter position amount:");
+    stdin
+        .read_line(&mut amount_input)
+        .expect("Failed to read amount");
+
+    println!("Enter position value:");
+    stdin
+        .read_line(&mut value_input)
+        .expect("Failed to read amount");
+
+    let name = name_input.trim().to_string();
+    let amount = amount_input
+        .trim()
+        .parse::<f64>()
+        .expect("Failed to parse amount to f64");
+    let value = value_input
+        .trim()
+        .parse::<f64>()
+        .expect("Failed to parse amount to f64");
+
+    Position {
+        name,
+        amount,
+        value,
+        buy_price: (value / amount),
+        sell_price: 0f64,
+        income: 0f64,
+    }
+}
+
+fn show_positions_table(positions: Vec<Position>) {
+    println!(
+        "{0: <10} | {1: <10} | {2: <10} | {3: <18} | {4: <10} | {5: <10} |",
+        "Name", "Amount", "Value", "Buy price", "Sell price", "Income"
+    );
+
+    positions.iter().for_each(|position| {
+        println!(
+            "{0: <10} | {1: <10} | {2: <10} | {3: <10} | {4: <10} | {5: <10} |",
+            position.name,
+            position.amount,
+            position.value,
+            position.buy_price,
+            position.sell_price,
+            position.income
+        )
+    });
+}
+
+fn main() {
+    let mut positions: Vec<Position> = vec![];
+
+    let position = get_position_info();
+    positions.push(position);
+
+    show_positions_table(positions);
+}
