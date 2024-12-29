042b29d7ec75eae103e7b36fcda7c8590b6efcc9
HerbyZ <andreyherby@gmail.com>
2024-12-30 02:06:11 +0300

    Position new method

--- src/models.rs
+++ src/models.rs
@@ -25,3 +25,42 @@ pub enum OrderType {
     Long,
     Short,
 }
+
+impl Position {
+    pub fn new(id: i32, name: String, mut orders: Vec<PositionOrder>) -> Position {
+        orders.sort_by(|first, second| first.id.cmp(&second.id));
+        let pos_type = orders.first().unwrap().clone().order_type;
+
+        let mut amount = 0f64;
+        let mut value = 0f64;
+
+        orders.iter().for_each(|order| {
+            if pos_type == order.order_type {
+                amount += order.amount;
+                value += order.value;
+            } else {
+                amount -= order.amount;
+                value -= order.value;
+            }
+        });
+
+        let avg_price = value / amount;
+        let mut income = 0f64;
+
+        orders.iter().for_each(|order| {
+            if pos_type != order.order_type {
+                income += avg_price - order.price;
+            }
+        });
+
+        Position {
+            id,
+            name,
+            position_type: pos_type,
+            amount,
+            avg_price,
+            income,
+            value,
+        }
+    }
+}
