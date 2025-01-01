f8aad6e7846a75e86effa7bdcac77a8677dd9078
HerbyZ <andreyherby@gmail.com>
2025-01-01 23:02:10 +0300

    Add order to position command

--- src/commands/position.rs
+++ src/commands/position.rs
@@ -1,5 +1,7 @@
 use crate::drawers::position::PositionDrawer;
-use crate::models::Position;
+use crate::models::{Action, Order, Position};
+use crate::utils::console::ask_for_input;
+use crate::{exit_with_error, storage};
 
 use super::CommandResult;
 
@@ -20,11 +22,65 @@ impl PositionHandler {
     pub fn handle_command(&mut self, command: String) -> CommandResult {
         match command.trim() {
             "q" => CommandResult::ChangeEditMode(super::EditMode::Global),
+            "a" => self.handle_add_order(),
             "n" => self.handle_next_page(),
             "p" => self.handle_previous_page(),
             _ => CommandResult::CommandNotFound,
             // TODO: Add position handler commands
         }
+    }
+
+    pub fn handle_add_order(&mut self) -> CommandResult {
+        let action_input = match ask_for_input::<String>("Enter order type (long/short)") {
+            Ok(value) => value,
+            Err(error) => return CommandResult::Error(error),
+        };
+
+        let action = match Action::from_string(action_input) {
+            Ok(action) => action,
+            Err(error) => return CommandResult::Error(error),
+        };
+
+        let amount = match ask_for_input::<f64>("Enter position amount") {
+            Ok(value) => value,
+            Err(error) => return CommandResult::Error(error),
+        };
+
+        let value = match ask_for_input::<f64>("Enter position value") {
+            Ok(value) => value,
+            Err(error) => return CommandResult::Error(error),
+        };
+
+        let price = value / amount;
+
+        let income = if self.position.action != action {
+            (price - self.position.avg_price) * amount
+        } else {
+            0f64
+        };
+
+        let id = self
+            .position
+            .orders
+            .last()
+            .expect("get last order of position")
+            .id
+            + 1;
+
+        self.position.orders.push(Order {
+            id,
+            action,
+            amount,
+            value,
+            price,
+            income,
+        });
+        self.drawer.set_position(self.position.clone());
+        if let Err(error) = storage::save_position(self.position.clone()) {
+            exit_with_error(error);
+        };
+
+        CommandResult::Ok
     }
 
     pub fn handle_next_page(&mut self) -> CommandResult {
