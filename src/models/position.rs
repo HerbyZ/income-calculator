106e84cd26faad9f692939344d3484e71c87d123
HerbyZ <andreyherby@gmail.com>
2025-01-08 09:03:50 +0300

    Position remove order method & its test

--- src/models/position.rs
+++ src/models/position.rs
@@ -72,6 +72,32 @@ impl Position {
         self.income = new_income;
         self.orders = new_orders;
     }
+
+    pub fn remove_order(&mut self, order_id: i32) -> Result<(), String> {
+        if order_id == 0 {
+            return Err(String::from("Cannot remove first order"));
+        }
+
+        let order_index = match self.orders.iter().position(|order| order.id == order_id) {
+            Some(order) => order,
+            None => {
+                return Err(format!(
+                    "Order with id {} not found in position {}",
+                    order_id, self.id
+                ))
+            }
+        };
+
+        let mut new_orders = self.orders.clone();
+        new_orders.remove(order_index);
+
+        self.orders = vec![];
+        new_orders
+            .iter()
+            .for_each(|order| self.add_order(order.to_owned()));
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -117,5 +143,48 @@ mod tests {
         assert_eq!(position.avg_value, 75f64);
         assert_eq!(position.avg_price, 7.5f64);
         assert_eq!(position.income, -25f64);
+    }
+
+    #[test]
+    fn test_position_remove_order() {
+        let first_order = Order {
+            id: 0,
+            action: Action::Long,
+            amount: 10f64,
+            value: 100f64,
+            price: 10f64,
+            income: 0f64,
+        };
+
+        let mut position = Position::new(0, String::from("MOCK"), vec![first_order]);
+
+        position.add_order(Order {
+            id: 1,
+            action: Action::Long,
+            amount: 10f64,
+            value: 50f64,
+            price: 5f64,
+            income: 0f64,
+        });
+
+        let tested_order_id = 2;
+        position.add_order(Order {
+            id: tested_order_id,
+            action: Action::Short,
+            amount: 10f64,
+            value: 50f64,
+            price: 5f64,
+            income: 5f64,
+        });
+
+        // Remove last order and check recalculation
+        position
+            .remove_order(tested_order_id)
+            .expect("Remove test order");
+
+        assert_eq!(position.amount, 20f64);
+        assert_eq!(position.avg_value, 150f64);
+        assert_eq!(position.avg_price, 7.5f64);
+        assert_eq!(position.income, 0f64);
     }
 }
