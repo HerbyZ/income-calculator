84acae71b7a21805878c06b431355066378f2859
HerbyZ <andreyherby@gmail.com>
2025-01-09 15:58:24 +0300

    Add "b", "buy" and "sell" options to action convertion from string

--- src/models/action.rs
+++ src/models/action.rs
@@ -7,8 +7,8 @@ pub enum Action {
 impl Action {
     pub fn from_string(string: String) -> Result<Action, String> {
         match string.to_lowercase().as_str() {
-            "l" | "long" => Ok(Action::Long),
-            "s" | "short" => Ok(Action::Short),
+            "l" | "long" | "b" | "buy" => Ok(Action::Long),
+            "s" | "short" | "sell" => Ok(Action::Short),
             _ => {
                 return Err(format!(
                     "'{}' is not valid position type (long/short)",
