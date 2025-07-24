fbd9613952b75eb83124ca284262b3f1352448e7
HerbyZ <andreyherby@gmail.com>
2025-07-24 19:16:11 +0300

    Remove unused constant STORAGE_FILE_PATH

--- src/constants.rs
+++ src/constants.rs
@@ -1,5 +1,2 @@
-// Path, where app will look for storage file (should contain filename)
-pub const STORAGE_FILE_PATH: &str = "./storage.json";
-
 // Path, where app will look for options file (should contain filename)
 pub const OPTIONS_FILE_PATH: &str = "./options.json";
